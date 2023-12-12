80419e77b308cc1fc574a5bf5343f407e1222dac
Miguel Cadaviz <miguel.cadaviz@farfetch.com>
2023-12-12 16:20:32 +0000

    feat: Create initial project structure

---
+++ src/CadavizCodeHub.Infrastructure/DependencyInjection/ServicesExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CadavizCodeHub.Domain.Entities;
+using CadavizCodeHub.Domain.Repositories;
+using CadavizCodeHub.Infrastructure.Database;
+using CadavizCodeHub.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CadavizCodeHub.Infrastructure.DependencyInjection
+{
+    [ExcludeFromCodeCoverage]
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection ConfigureInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddRepositories(configuration);
+
+            ConfigureDatabase();
+
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+        {
+            var dbSettings = configuration.GetRequiredSection("DatabaseSettings").Get<DatabaseSettings>();
+            services.AddSingleton(dbSettings);
+
+            return services;
+        }
+
+        private static void ConfigureDatabase()
+        {
+        }
+    }
+}
